58a83316675ed3f2bc3458b8a55413a9293421fd
Kingsword <kingsword09@gmail.com>
2025-03-02 10:10:55 +0800

    🐛 [packages/ios-network-detect] fix publish error

--- packages/tauri-plugin-ios-network-detect/src/lib.rs
+++ packages/tauri-plugin-ios-network-detect/src/lib.rs
@@ -13,11 +13,13 @@ pub use error::{Error, Result};
 #[cfg(target_os = "ios")]
 use mobile::IosNetworkDetect;
 
+#[cfg(target_os = "ios")]
 /// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the ios-network-detect APIs.
 pub trait IosNetworkDetectExt<R: Runtime> {
   fn ios_network_detect(&self) -> &IosNetworkDetect<R>;
 }
 
+#[cfg(target_os = "ios")]
 impl<R: Runtime, T: Manager<R>> crate::IosNetworkDetectExt<R> for T {
   fn ios_network_detect(&self) -> &IosNetworkDetect<R> {
     self.state::<IosNetworkDetect<R>>().inner()
@@ -28,9 +30,11 @@ impl<R: Runtime, T: Manager<R>> crate::IosNetworkDetectExt<R> for T {
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
   Builder::new("ios-network-detect")
     .setup(|app, api| {
-      #[cfg(target_os = "ios")]
-      let ios_network_detect = mobile::init(app, api)?;
-      app.manage(ios_network_detect);
+      #[cfg(target_os = "ios")] {
+        let ios_network_detect = mobile::init(app, api)?;
+        app.manage(ios_network_detect);
+      }
+      
       Ok(())
     })
     .build()
